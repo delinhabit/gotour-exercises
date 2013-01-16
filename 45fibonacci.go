20a9481d58958b193999ec728c5cf43db46e1973
Ion Scerbatiuc <delinhabit@gmail.com>
2013-01-16 15:43:05 +0200

    Refactored the fibonacci solution

--- 45fibonacci.go
+++ 45fibonacci.go
@@ -5,13 +5,10 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-	an2 := 0
-	an1 := 1
+	x, y := 0, 1
 	return func() int {
-		ret := an1
-		an := an1 + an2
-		an2 = an1
-		an1 = an
+		ret := x
+		x, y = y, x + y
 		return ret
 	}
 }
